3fd151c621c97d72dc24a784492f320fd910f370
ummesadeka <sadekaahmed53@gmail.com>
2022-01-22 11:52:50 +0600

    learning basic components

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I'm in love with React Native</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <Text style={styles.text}>I'm in love with React Native</Text>
+      <Button title="Lets start" onPress={()=> {Linking.openURL('https://reactnative.dev/docs/getting-started')}}></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f0',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    fontStyle: 'italic',
+    margin: 10,
   },
 });
