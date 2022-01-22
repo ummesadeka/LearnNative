f77ba1c7cc32b1a163aa3be39149ca6dd7dd35cd
ummesadeka <sadekaahmed53@gmail.com>
2022-01-22 21:53:50 +0600

    Learn StyleSheet

--- App.js
+++ App.js
@@ -2,38 +2,38 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('React Native');
-  const [ session, setSession ] = useState({ number: 6, title: 'state'});
-  const [ current, setCurrent ] = useState(true)
+  const [name, setName] = useState('Style Test');
+
 
   const onClickHandler = () => {
-    setName(' I am in love with React Native')
+    setName('Style Test is Done')
 
-    setSession({ number: 7, title: 'style' })
 
-    setCurrent(false);
   }
   return (
-    <View style={styles.body}>
+    <View style={{
+      height: '50%',
+      width: '100%',
+    backgroundColor: '#006000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: '#006060', 
+    borderRadius: 10,
+  }}>
       <Text style={styles.text}>{name} </Text>
-      <Text style={styles.text}> This is session number {session.number} and about {session.title} </Text>
-      <Text style={styles.text}> {current ? 'current session' : 'next session'} </Text>
       <Button title="Update State" onPress={onClickHandler}></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    backgroundColor: '#006000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
   text: {
     color: '#fff',
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
+    textTransform: 'uppercase'
   },
 });
