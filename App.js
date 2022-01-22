a695db15e0168bf89c61355957e898f6d3ef05a4
ummesadeka <sadekaahmed53@gmail.com>
2022-01-22 19:35:43 +0600

    Using the state hook

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Linking } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('React Native');
+  const [ session, setSession ] = useState({ number: 6, title: 'state'});
+  const [ current, setCurrent ] = useState(true)
+
+  const onClickHandler = () => {
+    setName(' I am in love with React Native')
+
+    setSession({ number: 7, title: 'style' })
+
+    setCurrent(false);
+  }
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>I'm in love with React Native</Text>
-      <Button title="Lets start" onPress={()=> {Linking.openURL('https://reactnative.dev/docs/getting-started')}}></Button>
+      <Text style={styles.text}>{name} </Text>
+      <Text style={styles.text}> This is session number {session.number} and about {session.title} </Text>
+      <Text style={styles.text}> {current ? 'current session' : 'next session'} </Text>
+      <Button title="Update State" onPress={onClickHandler}></Button>
     </View>
   );
 }
@@ -13,12 +26,12 @@ export default function App() {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#0f0',
+    backgroundColor: '#006000',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: 'black',
+    color: '#fff',
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
