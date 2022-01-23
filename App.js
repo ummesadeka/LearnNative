013a45a2089822c9982f32c5309bc7fe97115c40
ummesadeka <sadekaahmed53@gmail.com>
2022-01-23 11:45:24 +0600

    flexbox

--- App.js
+++ App.js
@@ -11,29 +11,53 @@ export default function App() {
 
   }
   return (
-    <View style={{
-      height: '50%',
-      width: '100%',
-    backgroundColor: '#006000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 10,
-    borderColor: '#006060', 
-    borderRadius: 10,
-  }}>
-      <Text style={styles.text}>{name} </Text>
-      <Button title="Update State" onPress={onClickHandler}></Button>
+    <View style={styles.body}>
+      <View style={styles.View1}>
+          <Text style={styles.text}> 1 </Text>
+      </View>
+      <View style={styles.View2}>
+          <Text style={styles.text}> 2 </Text>
+      </View>
+      <View style={styles.View3}>
+          <Text style={styles.text}> 3 </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- 
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#006000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  View1: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#0060f0',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  View2: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#f06060',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  View3: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#ff9060',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   text: {
     color: '#fff',
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
-    textTransform: 'uppercase'
   },
 });
